397e64fd96ae5cfb05ea6bd9c0160fc69f037b42
Freddie Nelson <freddie0208@hotmail.com>
2021-12-11 18:24:14 +0000

    add helper functions to aabb

--- src/physics/aabb.ts
+++ src/physics/aabb.ts
@@ -1,6 +1,7 @@
 import { vec2 } from "gl-matrix";
 import Shape from "../shapes/shape";
 import Collider from "./collider/collider";
+import CollisionObject from "./collisionObject";
 
 /**
  * Represents an axis aligned bounding box in 2D world space.
@@ -12,11 +13,9 @@ export default class AABB {
   max: vec2;
 
   /**
-   * Creates a {@link AABB} that bounds the vertices of the given collider.
-   *
-   * @param c The collider to create AABB from
+   * A {@link CollisionObject} that is bound by the {@link AABB}.
    */
-  constructor(c: Collider);
+  obj: CollisionObject;
 
   /**
    * Creates a {@link AABB} with the given minimum and and maximum vertices.
@@ -26,16 +25,42 @@ export default class AABB {
    */
   constructor(min: vec2, max: vec2);
 
-  constructor(min: Collider | vec2, max?: vec2) {
-    if (min instanceof Collider) {
-      this.setMinMaxFromCollider(min);
+  /**
+   * Creates an {@link AABB} which contains both `a` and `b`, the created {@link AABB} is the union of `a` and `b` plus the `margin`.
+   *
+   * @param a An {@link AABB}
+   * @param b An {@link AABB}
+   * @param margin A margin to add to the min and max of the created AABB
+   */
+  constructor(a: AABB, b: AABB, margin: number);
+
+  /**
+   * Creates a {@link AABB} that bounds the vertices of the given object's collider.
+   *
+   * @param obj {@link CollisionObject} to create AABB from
+   */
+  constructor(obj: CollisionObject);
+
+  constructor(min: CollisionObject | vec2 | AABB, max?: vec2 | AABB, margin?: number) {
+    if (min instanceof CollisionObject) {
+      this.setMinMaxFromCollisionObj(min);
+    } else if (typeof margin !== "undefined") {
+      this.union(<AABB>min, <AABB>max, margin);
     } else {
-      this.min = min;
-      this.max = max;
+      this.min = <vec2>min;
+      this.max = <vec2>max;
     }
   }
 
-  setMinMaxFromCollider(c: Collider) {
+  /**
+   * Sets the AABB's min and max so that the AABB contains the object's collider.
+   *
+   * @param obj The {@link CollisionObject} to bound
+   */
+  setMinMaxFromCollisionObj(obj: CollisionObject) {
+    this.obj = obj;
+
+    const c = obj.collider;
     const vertices = c.getVertices();
 
     let minX = Infinity;
@@ -60,5 +85,44 @@ export default class AABB {
 
     this.min = vec2.fromValues(minX, minY);
     this.max = vec2.fromValues(maxX, maxY);
+  }
+
+  /**
+   * Sets the AABB's min and max to the union of `a` and `b`.
+   *
+   * @param a An {@link AABB}
+   * @param b An {@link AABB}
+   * @param margin A margin to add to the new min and max points
+   */
+  union(a: AABB, b: AABB, margin = 0) {
+    const minX = a.min[0] < b.min[0] ? a.min[0] : b.min[0];
+    const maxX = a.max[0] > b.max[0] ? a.max[0] : b.max[0];
+    const minY = a.min[1] < b.min[1] ? a.min[1] : b.min[1];
+    const maxY = a.max[1] > b.max[1] ? a.max[1] : b.max[1];
+
+    this.min = vec2.fromValues(minX - margin, minY - margin);
+    this.max = vec2.fromValues(maxX + margin, maxY + margin);
+  }
+
+  /**
+   * Determines wether or not this {@link AABB} intersects with the given {@link AABB}.
+   *
+   * @param b The {@link AABB} to check for intersection against
+   */
+  intersects(b: AABB) {
+    return (
+      this.max[0] > b.min[0] && this.min[0] < b.max[0] && this.max[1] > b.min[1] && this.min[1] < b.max[1]
+    );
+  }
+
+  /**
+   * Determines wether or not the given {@link AABB} is contained within this {@link AABB}.
+   *
+   * @param b The {@link AABB} to check
+   */
+  contains(b: AABB) {
+    return (
+      this.min[0] <= b.min[0] && this.max[0] >= b.max[0] && this.min[1] <= b.min[1] && this.max[1] >= b.max[1]
+    );
   }
 }
